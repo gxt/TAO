3b2fb1c190d1ece643b8984edd12f24fe588180c
Guan Xuetao <gxt@pku.edu.cn>
2019-01-13 11:21:01 +0800

    Combine mygit.sh to TAO-git.sh
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- bin/TAO-git.sh
+++ bin/TAO-git.sh
@@ -120,6 +120,39 @@ c_push()
 	esac
 }
 
+PATCHDIR=~/patches/
+PATCHVER=v5             # changed by time
+
+c_oldgit()
+{
+	go_with_exit todo
+	if [ $1 == "format-patch" ]; then
+	    git format-patch --thread --cover-letter --subject-prefix=PATCH$PATCHVER    \
+	        --output-directory $PATCHDIR/qemu-uc32-$PATCHVER                        \
+	        --cc=blauwirbel@gmail.com                                               \
+	        --cc=afaerber@suse.de                                                   \
+	        --cc=chenwj@iis.sinica.edu.tw                                           \
+	        master..unicore32
+	    ./scripts/checkpatch.pl $PATCHDIR/qemu-uc32-$PATCHVER/*                     \
+	        > $PATCHDIR/qemu-uc32-$PATCHVER/checkpatch
+
+	elif [ $1 == "request-pull" ]; then
+	    git request-pull master git://github.com/gxt/QEMU.git unicore32             \
+	        > $PATCHDIR/qemu-uc32-$PATCHVER/request-pull
+
+	elif [ $1 == "send-email" ]; then
+	    shift
+	    git send-email --from=gxt@mprc.pku.edu.cn                                   \
+	        --to=qemu-devel@nongnu.org                                              \
+	        --subject="[PULL] UniCore32 PUV3 machine support"                       \
+	        --cc=blauwirbel@gmail.com                                               \
+	        --cc=afaerber@suse.de                                                   \
+	        --cc=chenwj@iis.sinica.edu.tw                                           \
+	        --cc=gxt@mprc.pku.edu.cn                                                \
+	        $@
+	fi
+}
+
 if [ $# -ne 2 ]; then
 	go_with_exit usage
 fi
